8533a6291e326ace864dad6a3e8df96fb03c5e24
Vinícius Mioto <vsmioto@gmail.com>
2023-11-25 18:16:46 -0300

    Add Review

---
+++ include/model/Review.hpp
@@ -0,0 +1,33 @@
+#ifndef REVIEW_HPP
+#define REVIEW_HPP
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <string>
+
+namespace places {
+class Review {
+   public:
+    Review();
+    Review(const std::string &comment, const unsigned char rating,
+           const boost::posix_time::ptime &date);
+
+    virtual ~Review() = default;
+
+    const std::string &getComment() const;
+    void setComment(const std::string &comment);
+
+    unsigned char getRating() const;
+    void setRating(const unsigned char rating);
+
+    const boost::posix_time::ptime &getDate() const;
+    void setDate(const boost::posix_time::ptime &date);
+
+   private:
+    std::string comment;
+    unsigned char rating;
+    boost::posix_time::ptime date;
+};
+
+}  // namespace places
+
+#endif
