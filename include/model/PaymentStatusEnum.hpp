91797a6d4cc3e1a6ccf1d27ac815545ae96dc744
David Gomes <david.gomes@tuta.io>
2023-11-25 18:12:45 -0300

    Add payment enums

---
+++ include/model/PaymentStatusEnum.hpp
@@ -0,0 +1,14 @@
+#ifndef PAYMENT_STATUS_ENUM_HPP
+#define PAYMENT_STATUS_ENUM_HPP
+
+namespace places {
+
+enum PaymentStatusEnum {
+    CANCELED,
+    IN_PROGRESS,
+    SUCCESS,
+};
+
+} // namespace places
+
+#endif
