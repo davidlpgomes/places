03a62b47887db8b2ccc161897793b6d4e83e72da
David Gomes <david.gomes@tuta.io>
2023-11-26 16:40:33 -0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
+#include <set>
 
-#include "../include/model/User.hpp"
-#include "../include/model/Address.hpp"
+#include "../include/model/Database.hpp"
 
+using namespace places;
 
 int main() {
-    std::cout << "Hello, places!" << std::endl;
+    Database *db{Database::getInstance()};
 
-    places::Address address;
-    places::User user{"email", "pass", "phone", "name", address};
+    Person *p1{new Person{}};
+    Person *p2{new Person{}};
+    Person *p3{new Person{}};
 
-    std::cout << user.getCreationDate() << std::endl;
+    Event *e1{new Event{}};
+    Event *e2{new Event{}};
+    Event *e3{new Event{}};
+
+    EventIntention ei1{EventIntention{e2->getId(), p1->getId()}};
+    EventIntention ei2{EventIntention{e3->getId(), p2->getId()}};
+    EventIntention ei3{EventIntention{e1->getId(), p3->getId()}};
+
+    db->addPerson(p1);
+    db->addPerson(p2);
+    db->addPerson(p3);
+
+    db->addEvent(e1);
+    db->addEvent(e2);
+    db->addEvent(e3);
+
+    db->addEventIntetion(ei1);
+    db->addEventIntetion(ei2);
+    db->addEventIntetion(ei3);
+
+    const std::multiset<EventIntention> *eis{db->getEventIntetions()};
+
+    std::multiset<EventIntention>::const_iterator it{eis->begin()};
+
+    for (; it != eis->end(); ++it)
+        std::cout << "e=" << (*it).getEventId() << " p=" << (*it).getPersonId() << std::endl;
 
     return 0;
 }
