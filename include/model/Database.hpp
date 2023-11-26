43de1f0e7917ce6df97db7ca972c6d098fbee572
David Gomes <david.gomes@tuta.io>
2023-11-26 16:40:15 -0300

    Add database model

---
+++ include/model/Database.hpp
@@ -0,0 +1,71 @@
+#ifndef DATABASE_HPP
+#define DATABASE_HPP
+
+#include <set>
+#include <map>
+#include <vector>
+
+#include "Company.hpp"
+#include "Event.hpp"
+#include "EventIntention.hpp"
+#include "Follow.hpp"
+#include "Friendship.hpp"
+#include "Invite.hpp"
+#include "Person.hpp"
+#include "Place.hpp"
+
+
+namespace places {
+
+class Database {
+    public:
+        Database(Database &other) = delete;
+        void operator=(const Database &) = delete;
+
+        static Database *getInstance();
+
+        const std::map<int, Person*> *getPersons() const;
+        void addPerson(Person *const person);
+
+        const std::map<int, Company*> *getCompanies() const;
+        void addCompany(Company *const company);
+
+        const std::map<int, Event*> *getEvent() const;
+        void addEvent(Event *const event);
+
+        const std::map<int, Place*> *getPlaces() const;
+        void addPlace(Place *const place);
+
+        const std::vector<Follow*> *getFollows() const;
+        void addFollow(Follow *const follow);
+
+        const std::vector<Friendship*> *getFriendships() const;
+        void addFriendship(Friendship *const friendship);
+
+        const std::vector<Invite*> *getInvites() const;
+        void addInvite(Invite *const invite);
+
+        const std::multiset<EventIntention> *getEventIntetions() const;
+        void addEventIntetion(const EventIntention &intention);
+
+    private:
+        Database();
+        virtual ~Database() = default;
+
+        static Database *database;
+
+        std::map<int, Person*> *persons;
+        std::map<int, Company*> *companies;
+        std::map<int, Event*> *events;
+        std::map<int, Place*> *places;
+
+        std::vector<Follow*> *follows;
+        std::vector<Friendship*> *friendships;
+        std::vector<Invite*> *invites;
+
+        std::multiset<EventIntention> *intentions;
+};
+
+} // namespace places
+
+#endif
---
+++ src/model/Database.cpp
@@ -0,0 +1,113 @@
+#include "../../include/model/Database.hpp"
+
+
+using namespace places;
+
+Database *Database::database{nullptr};
+
+Database::Database():
+    persons{new std::map<int, Person*>},
+    companies{new std::map<int, Company*>},
+    events{new std::map<int, Event*>},
+    places{new std::map<int, Place*>},
+    follows{new std::vector<Follow*>},
+    friendships{new std::vector<Friendship*>},
+    invites{new std::vector<Invite*>},
+    intentions{new std::multiset<EventIntention>} {
+};
+
+Database *Database::getInstance() {
+    if (Database::database == nullptr)
+        Database::database = new Database{};
+
+    return Database::database;
+}
+
+const std::map<int, Person*> *Database::getPersons() const {
+    return this->persons;
+}
+
+void Database::addPerson(Person *const person) {
+    this->persons->insert(
+        std::pair<int, Person*>(person->getId(), person)
+    );
+
+    return;
+}
+
+const std::map<int, Company*> *Database::getCompanies() const {
+    return this->companies;
+}
+
+void Database::addCompany(Company *const company) {
+    this->companies->insert(
+        std::pair<int, Company*>(company->getId(), company)
+    );
+
+    return;
+}
+
+const std::map<int, Event*> *Database::getEvent() const {
+    return this->events;
+}
+
+void Database::addEvent(Event *const event) {
+    this->events->insert(
+        std::pair<int, Event*>(event->getId(), event)
+    );
+
+    return;
+}
+
+const std::map<int, Place*> *Database::getPlaces() const {
+    return this->places;
+}
+
+void Database::addPlace(Place *const place) {
+    this->places->insert(
+        std::pair<int, Place*>(place->getId(), place)
+    );
+
+    return;
+}
+
+const std::vector<Follow*> *Database::getFollows() const {
+    return this->follows;
+}
+
+void Database::addFollow(Follow *const follow) {
+    this->follows->push_back(follow);
+    
+    return;
+}
+
+const std::vector<Friendship*> *Database::getFriendships() const {
+    return this->friendships;
+}
+
+void Database::addFriendship(Friendship *const friendship) {
+    this->friendships->push_back(friendship);
+
+    return;
+}
+
+const std::vector<Invite*> *Database::getInvites() const {
+    return this->invites;
+}
+
+void Database::addInvite(Invite *const invite) {
+    this->invites->push_back(invite);
+
+    return;
+}
+
+const std::multiset<EventIntention> *Database::getEventIntetions() const {
+    return this->intentions;
+}
+
+void Database::addEventIntetion(const EventIntention &intention) {
+    this->intentions->insert(intention);
+
+    return;
+}
+
